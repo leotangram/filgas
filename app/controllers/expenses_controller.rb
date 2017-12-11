1c814dd51049371b70f40814e790d404d8974a8e
leotangram <leotangram@gmail.com>
2017-12-10 20:31:53 -0500

    filtrando gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+     @expenses = @expenses.where("concept LIKE :concept", concept: "%#{params[:concept]}%")
+     @expenses = @expenses.where("category_id LIKE :category_id", category_id: "%#{params[:category_id]}%")
   end
 end
